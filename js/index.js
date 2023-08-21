2e47315d5ae8c4484c4dd6635c0eaa74255de551
Lamia Jabin Rimty <lamiajabinrimty@Lamias-Air.home>
2023-08-21 23:28:40 +0200

    added Footer

--- js/index.js
+++ js/index.js
@@ -92,3 +92,25 @@ tabs.forEach(tab =>{
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+// Scroll active link
+  const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
